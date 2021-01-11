48eb79c95ba2f45560af56b5d050030a3e76d1d2
Shynn Lawrence <61219106+shua1090@users.noreply.github.com>
2021-01-10 17:29:14 -0800

    Reworked GUI and added concurrent keygen tracker

--- pgpf/RSA.java
+++ pgpf/RSA.java
@@ -1,6 +1,6 @@
-import java.math.*;
+package pgpf;
+import java.math.BigInteger;
 import java.util.Random;
-import java.util.*;
 /*
     Provides encryption services for text
     Copyright (C) 2021 Shynn Lawrence
@@ -18,30 +18,15 @@ import java.util.*;
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
-class RSA{
+public class RSA{
     BigInteger e; // Public Key
     BigInteger d; // Private Key
     BigInteger n; 
     RSA(){
-        setup();
-    }
-
-    private void setup(){
-        BigInteger f = largePrime(256);
-        BigInteger k = largePrime(256);
-        this.n = f.multiply(k);
-        var phi = (f.subtract(BigInteger.valueOf(1)).multiply(k.subtract(BigInteger.valueOf(1))));
-        BigInteger temp;
-        do{
-            temp = largePrime(250);
-        } while (!(temp.gcd(phi).compareTo(BigInteger.ONE) == 0)|| (temp.compareTo(f) != -1 || temp.compareTo(k) != -1));
-        this.e = temp;
-        this.d = inverse(phi, e);
-        System.out.println("Expected answer: 1; Actual answer: "+((this.e.multiply(this.d)).mod(phi)));
     }
 
     // Extended Euclidean Algorithm
-    BigInteger inverse(BigInteger a, BigInteger b){
+    public BigInteger inverse(BigInteger a, BigInteger b){
         BigInteger inv = BigInteger.ZERO;
         BigInteger q, r, r1 = a, r2 = b, t, t1 = BigInteger.ZERO, t2 = BigInteger.ONE;
         while (r2.compareTo(BigInteger.ZERO) == 1){
@@ -62,7 +47,8 @@ class RSA{
         return inv;
     }
 
-    private BigInteger largePrime(int size){
+    // Partial Pseudo-Random Prime Number Generation
+    public BigInteger largePrime(int size){
         BigInteger longVal = BigInteger.valueOf(Long.MAX_VALUE);
         Random rand = new Random();
         BigInteger result;
@@ -71,48 +57,25 @@ class RSA{
         byteArray[0] = 0;
         result = new BigInteger(byteArray);
         result = result.nextProbablePrime();
-        // Pure random
-        // do{
-            // rand.nextBytes(byteArray);
-            // result = new BigInteger(byteArray);
-        // } while (result.compareTo(longVal) != 1 || !result.isProbablePrime(1));
+        // FIXME: Pure random - Better but takes longer time. Still deciding.
+        do{
+            rand.nextBytes(byteArray);
+            result = new BigInteger(byteArray);
+        } while (result.compareTo(longVal) != 1 || !result.isProbablePrime(1));
         return result;
     }
 
     public void gen(){
-    }
-}
-
-class keygen{
-    int encryptionNum;
-    keygen(String encryption){
-        switch (encryption){
-            case "rsa":
-                this.encryptionNum = 1;
-                break;
-            case "des":
-                this.encryptionNum = 2;
-                break;
-            default:
-                this.encryptionNum = 0;
-                break;
-        }
-    }
-
-
-    public void generate(){
-        if (encryptionNum == 1){
-            var r = new RSA();
-        }
-    }
-}
-
-public class pgp{   
-    public static void main(String[] args){
-        for (int i = 0; i < 10; i++){
-            long t = new Date().getTime();
-            new keygen("rsa").generate();
-            System.out.println(String.format("%d time has passed.", new Date().getTime()-t));
-        }
+        BigInteger f = largePrime(256);
+        BigInteger k = largePrime(256);
+        this.n = f.multiply(k);
+        var phi = (f.subtract(BigInteger.valueOf(1)).multiply(k.subtract(BigInteger.valueOf(1))));
+        BigInteger temp;
+        do{
+            temp = largePrime(250);
+        } while (!(temp.gcd(phi).compareTo(BigInteger.ONE) == 0)|| (temp.compareTo(f) != -1 || temp.compareTo(k) != -1));
+        this.e = temp;
+        this.d = inverse(phi, e);
+        System.out.println("Expected answer: 1; Actual answer: "+((this.e.multiply(this.d)).mod(phi)));
     }
 }
---
+++ pgpf/pgp.java
@@ -0,0 +1,189 @@
+package pgpf;
+
+import java.io.File;
+import java.math.BigInteger;
+import java.util.Date;
+
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+/*
+    Pretty Good Privacy - Provides encryption services
+    Copyright (C) 2021 Shynn Lawrence
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+public class pgp extends Thread {
+    JTabbedPane pgpPanes;
+    JFrame mainFrame;
+
+    @Override
+    public synchronized void start() {
+        
+    }
+
+    // RSA
+    JTabbedPane rsaPane = new JTabbedPane();
+    static RSA rsaBase = new RSA();
+    JPanel rsaGenerate = new JPanel();
+
+    public static void genRSA(){
+        new Thread(new Runnable(){
+            @Override
+            // RSA Generation with Helpful Progress updates
+            public void run() {
+                long startTime = new Date().getTime();
+                JFrame window = new JFrame();
+                window.setSize(400, 300);
+                window.setVisible(true);
+                window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                window.setResizable(false);
+                window.setTitle("RSA Generation");
+                
+                var textArea = new JTextArea("", 5, 50);
+                textArea.setLineWrap(true);
+                textArea.setEditable(false);
+                textArea.setVisible(true);
+                
+                var textPanel = new JScrollPane(textArea);
+                textPanel.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+                textPanel.setVisible(true);
+                window.add(textPanel);
+
+                window.setVisible(true);
+                textArea.append("-------RSA-------\n");
+
+                textArea.append("-Generating a large Prime Number-\n");
+                textArea.setCaretPosition(textArea.getDocument().getLength());
+
+                BigInteger f = rsaBase.largePrime(256);
+
+                textArea.append(f.toString());
+                textArea.append("\nFound");
+                textArea.setCaretPosition(textArea.getDocument().getLength());
+
+                textArea.append("\n-Generating another large Prime Number-\n");
+                textArea.setCaretPosition(textArea.getDocument().getLength());
+
+                BigInteger k = rsaBase.largePrime(256);
+
+                textArea.append(k.toString());
+                textArea.append("\nFound");
+                
+                textArea.append("\nCalculating N");
+                rsaBase.n = f.multiply(k);
+                textArea.append("\nCalculated n");
+                textArea.setCaretPosition(textArea.getDocument().getLength());
+                
+                textArea.append("\nCalculating φ");
+                BigInteger phi = (f.subtract(BigInteger.valueOf(1)).multiply(k.subtract(BigInteger.valueOf(1))));
+                textArea.append("\nCalculated φ");
+                textArea.setCaretPosition(textArea.getDocument().getLength());
+
+                textArea.append("\nCalculating Public (e) key");
+                BigInteger temp;
+                do{
+                    temp = rsaBase.largePrime(255);
+                } while (!(temp.gcd(phi).compareTo(BigInteger.ONE) == 0)|| (temp.compareTo(f) != -1 || temp.compareTo(k) != -1));
+                textArea.append("\nCalculated Public key");
+                textArea.setCaretPosition(textArea.getDocument().getLength());
+                rsaBase.e = temp;
+                textArea.append("\nCalculating Private Key using Extended Euclidean Algorithm");
+                rsaBase.d = rsaBase.inverse(phi, rsaBase.e);
+                textArea.append("\nCalculated Private key");
+                textArea.setCaretPosition(textArea.getDocument().getLength());
+
+                // textArea.append("\nCombining N with E and D");
+                // try{Thread.sleep(1000);} catch (Exception e) {}
+                // textArea.append("\nCompleted.");
+
+                textArea.append("\nSanity Check:");
+                textArea.append("\nExpected answer: 1; Actual answer: "+((rsaBase.e.multiply(rsaBase.d)).mod(phi)));
+                textArea.setCaretPosition(textArea.getDocument().getLength());
+
+                // try{Thread.sleep(5000);} catch (Exception e) {}
+                // textArea.append("\nIf 'Actual Answer' is not 1, the RSA process failed. \nPlease report this incident and try again.");
+                // textArea.setCaretPosition(textArea.getDocument().getLength());
+                textArea.append("\nThe size of the public key is: "+rsaBase.e.toString(2).length() + " bits long");
+                textArea.append("\nThe size of the private key is: "+rsaBase.d.toString(2).length() + " bits long");
+                textArea.append("\nThe RSA Generation has complete in " + (new Date().getTime() - startTime) / 1000+ " seconds.\n This window will automatically close in 10 seconds.\n");
+                textArea.setCaretPosition(textArea.getDocument().getLength());
+                try{Thread.sleep(10000);} catch (Exception e) {}
+                window.setEnabled(false);
+                window.setVisible(false);
+            }
+        }).start();
+
+    }
+
+
+    private void setupRSA() {
+        var gl = new GridLayout(10, 10);
+        // this.rsaGenerate = new JPanel();
+        rsaPane.add("Generate", this.rsaGenerate);
+        this.rsaGenerate.setLayout(gl);
+
+        class ButtonListener implements ActionListener {
+            @Override
+            public void actionPerformed(ActionEvent eventPushed) {
+                if (eventPushed.getActionCommand().equals("Generate")) {
+                    try {
+                        genRSA();
+                    } catch (Exception e1) {
+                        e1.printStackTrace();
+                    }
+                }
+            }
+        }
+
+        JButton j = new JButton("Generate");
+        j.setPreferredSize(new Dimension(100, 50));
+        this.rsaGenerate.add(j);
+        j.addActionListener(
+            new ButtonListener()
+        );
+    }
+
+    pgp(){
+        mainFrame = new JFrame();
+        
+        pgpPanes = new JTabbedPane();
+
+        mainFrame.setTitle("PGP");
+
+        try {
+            File pathToFile = new File("pgpf/pgplogo.png");
+            Image image = ImageIO.read(pathToFile);
+            mainFrame.setIconImage(image);
+        } catch (Exception ex) {
+            System.out.println(ex.getStackTrace());
+        }
+        mainFrame.setSize(500, 400);
+        mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        mainFrame.setResizable(false);
+        
+        pgpPanes.addTab("RSA", rsaPane);
+        
+        setupRSA();
+        
+        mainFrame.add(pgpPanes);
+        mainFrame.setVisible(true);
+    }
+
+    public static void main(String[] args){
+        new pgp();
+    }
+}
